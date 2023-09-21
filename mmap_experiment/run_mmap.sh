7ccecec7298de947230ba181b9778092e13aecb5
Altanali Nagji <altannag@node-0.altannag.os-class.emulab.net>
2023-09-21 17:16:28 -0600

    feat: Create Experiment 1 Run Script

---
+++ mmap_experiment/run_mmap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+export TIMEFORMAT=%R
+num_runs=10
+program="time ~/AdvancedOSProject1/mmap_experiment/mmap_exec"
+program_data='~/AdvancedOSProject1/data.txt'
+output_file="results.txt"
+
+
+> "$output_file"
+
+for i in {0..1}; do
+
+	for j in {0..1}; do
+		flags="-f $program_data"
+		experiment_type=""
+		#File private/shared flag
+		if [[ $i -eq 0 ]]; then
+			experiment_type+="Private "
+		else
+			flags+=" -s"
+			experiment_type+="Shared "
+		fi
+		
+		#File-backed/anonymous flag
+		if [[ $j -eq 0 ]]; then
+			experiment_type+="File-Backed"
+		else
+			flags+=" -a"
+			experiment_type+="Anonymous"
+		fi
+
+		echo "$experiment_type" | tee -a "$output_file"
+		for ((k=0; k<num_runs; k++)); do
+			eval "$program $flags" 2>> "$output_file"
+		done
+	done
+done
